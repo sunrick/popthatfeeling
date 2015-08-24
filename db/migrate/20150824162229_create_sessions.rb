2b3bbb131a17b405f8bb213f304ea0085283d035
Rickard Sunden <rickard.sunden@outlook.com>
2015-08-24 13:20:47 -0400

    Removed start_time

--- db/migrate/20150824162229_create_sessions.rb
+++ db/migrate/20150824162229_create_sessions.rb
@@ -1,7 +1,6 @@
 class CreateSessions < ActiveRecord::Migration
   def change
     create_table :sessions do |t|
-      t.datetime :start_time
       t.datetime :end_time
       t.timestamps null: false
     end
