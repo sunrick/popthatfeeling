3cbc45f81ca68a7eee5d5da11961cccc3e0e1394
Rickard Sunden <rickard.sunden@outlook.com>
2015-09-14 22:06:00 -0400

    Moved stats functions

---
+++ app/assets/javascripts/stats.js
@@ -0,0 +1,61 @@
+// FUNCTIONS FOR USER STATS UPDATING
+
+// chrome and other browsers don't start with focus, firefox does.
+var initTimer;
+var timer;
+
+// initial run of stats before timer kicks in
+lets_count('current_sessions', 'person');
+lets_count('feelings_today', 'cloud');
+
+initTimer = setInterval(function(){
+                  lets_count('current_sessions', 'person');
+                  lets_count('feelings_today', 'cloud');
+                  console.log("timerworksinit");
+                }, 20000);
+
+$(window).on("blur focus", function(e) {
+  var prevType = $(this).data("prevType");
+
+  if (prevType != e.type) {   //  reduce double fire issues
+    switch (e.type) {
+      case "blur":
+        clearInterval(initTimer);
+        clearInterval(timer);
+      break;
+      case "focus":
+        clearInterval(initTimer);
+        timer = setInterval(function(){
+                  lets_count('current_sessions', 'person');
+                  lets_count('feelings_today', 'cloud');
+                  console.log("timerworks");
+                }, 20000);
+      break;
+    }
+  }
+  $(this).data("prevType", e.type);
+})
+
+function lets_count(path, icon){
+  $.ajax({
+    url: "/stats/"+path,
+    method: "GET",
+    data: {},
+    dataType: "json",
+    success: function ( msg ) {
+      current_count(msg.count, icon);
+    }
+  });
+ }
+
+// check if counts have updated or not
+
+function current_count(count, icon){
+  var oldCount = +$("#"+icon +' span').text();
+  if (count !== oldCount){
+    $("#"+icon +' span').fadeOut(500,function(){
+    $("#"+icon +' span').text(count);
+   });
+    $("#"+icon +' span').fadeIn(500);
+  }
+}
