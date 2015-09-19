6f9da62a5ab45ac8b6ef44aa9db9c616dd770c9b
Rickard Sunden <rickard.sunden@outlook.com>
2015-09-18 20:20:26 -0400

    Added logic for sessions

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -1,10 +1,28 @@
 class FeelingsController < ApplicationController
 
   def create
-    if params[:text].downcase != "click me"
+    session = Session.find(params[:session_id])
+    session_finished = !session.end_time.nil?
+    last_feeling = session.feelings.last
+
+    if session_finished || (session.created_at < 5.minutes.ago && last_feeling.nil?)
+      if !session_finished
+        session.destroy
+      end
+      @session = Session.create.id
       Feeling.create(text: params[:text], session_id: params[:session_id])
+    elsif last_feeling.nil? || last_feeling.created_at > 5.minutes.ago
+      Feeling.create(text: params[:text], session_id: params[:session_id])
+      @session = params[:session_id].to_i
+    else
+      session.update(end_time: last_feeling.created_at)
+      @session = Session.create.id
+      Feeling.create(text: params[:text], session_id: @session)
     end
-    render nothing: true
+    render json: { id: @session }
   end
 
 end
+
+
+
