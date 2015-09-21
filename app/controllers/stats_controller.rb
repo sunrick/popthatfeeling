9db69575b95a047f43163d03df010c5d31434d7b
Rickard Sunden <rickard.sunden@outlook.com>
2015-09-21 09:45:14 -0400

    Removed uneeded methods

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,48 +2,8 @@ class StatsController < ApplicationController
   # Useful if you need to convert time into string in controller
   include ActionView::Helpers::DateHelper
 
-  def current_sessions
-    @sessions = Session.where(end_time: nil).count
-    render json: { count: @sessions}
-  end
-
-  def total_sessions
-    @sessions = Session.count
-    render json: { count: @sessions}
-  end
-
-  def sessions_today
-    @sessions = Session.where('created_at >= ?', Time.zone.now.beginning_of_day).count
-    render json: { count: @sessions}
-  end
-
-  def sessions_month
-    @sessions = Session.where('created_at >= ?', Time.zone.now.send.beginning_of_month).count
-    render json: { count: @sessions}
-  end
-
-  def sessions_year
-    @sessions = Session.where('created_at >= ?', Time.zone.now.send.beginning_of_year).count
-    render json: { count: @sessions}
-  end
-
-  def total_feelings
-    @feelings = Feeling.count
-    render json: { count: @sessions}
-  end
-
   def feelings_today
-    @sessions = Feeling.where('created_at >= ?', Time.zone.now.beginning_of_day).count
-    render json: { count: @sessions}
-  end
-
-  def feelings_month
-    @sessions = Feeling.where('created_at >= ?', Time.zone.now.send.beginning_of_month).count
-    render json: { count: @sessions}
-  end
-
-  def feelings_year
-    @sessions = Feeling.where('created_at >= ?', Time.zone.now.send.beginning_of_year).count
+    @sessions = Feeling.where('created_at >= ?', 24.hours.ago).count
     render json: { count: @sessions}
   end
 
