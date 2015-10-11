d233577819dc98ff878209cd2fbf5249d3bd1f39
Rickard Sunden <rickard.sunden@outlook.com>
2015-10-11 10:28:22 -0400

    Updated so sessions can't be super long

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,15 @@ class SessionsController < ApplicationController
   end
 
   def end
-    @session = Session.find_by(id: params[:session_id], end_time: nil).update(end_time: DateTime.now)
+    @session = Session.find_by(id: params[:session_id], end_time: nil)
+    @feeling = @session.feelings.last
+    if @feeling.created_at > 10.seconds.ago
+      @session.update(end_time: DateTime.now)
+    elsif @feeling.nil?
+      @session.destroy
+    else
+      @session.update(end_time: @feeling.created_at)
+    end
     render nothing: true
   end
 
